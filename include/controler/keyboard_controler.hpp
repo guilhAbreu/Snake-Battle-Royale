3c96db0100cc16e39df875a6536afbf161ddb3ac
Guilherme Abreu <guiii88@outlook.com>
2018-10-05 06:42:29 -0300

    add remote-keyboard code

--- include/controler/keyboard_controler.hpp
+++ include/controler/keyboard_controler.hpp
@@ -4,15 +4,28 @@
 #include <ncurses.h>
 #include <thread>
 
-void threadfun (int *keybuffer, int *control);
+/*Socket*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+void threadfun (int *keybuffer, int *control, int *socket_fd, int *connection_fd);
 
 class Teclado {
   private:
     int ultima_captura; // last capture
     int rodando; // running
-
     std::thread kb_thread; // keyboard thread
+    int socket_fd, connection_fd, portno;
+    struct sockaddr_in myself, client;
+    socklen_t client_size = (socklen_t)sizeof(client);
 
+    bool init_server(int port);
   public:
     Teclado();
     ~Teclado();
